772341ed4101b97d7c9973b0a111f407e3241e8a
enzoruizdiaz <enzojuliann1@gmail.com>
2021-09-08 15:09:01 -0300

    first commit

---
+++ restaurant/src/main/java/com/restaurant/models/Category.java
@@ -0,0 +1,5 @@
+package com.restaurant.models;
+
+public class Category {
+
+}
---
+++ restaurant/src/main/java/com/restaurant/models/Product.java
@@ -0,0 +1,15 @@
+package com.restaurant.models;
+
+import lombok.Data;
+
+@Data
+public class Product {
+	
+	
+	
+	
+	
+	
+
+	
+}
